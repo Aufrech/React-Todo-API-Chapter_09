9185568d721625559f610c26a6b1b71794d3a6e9
Aufrech <77332341+Aufrech@users.noreply.github.com>
2021-04-10 18:40:27 +0200

    --- Auto-Git Commit ---

---
+++ app/build/precache-manifest.a309269637f8787ccc0ec692b16cebc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ccc55c3b942eaf03814c8258c1a6b44b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d53360a15ce3d3a7782",
+    "url": "/static/css/main.2ca92a8c.chunk.css"
+  },
+  {
+    "revision": "cd63e9097c65ecde92ba",
+    "url": "/static/js/2.b257626e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b257626e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d53360a15ce3d3a7782",
+    "url": "/static/js/main.e711cd6b.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
